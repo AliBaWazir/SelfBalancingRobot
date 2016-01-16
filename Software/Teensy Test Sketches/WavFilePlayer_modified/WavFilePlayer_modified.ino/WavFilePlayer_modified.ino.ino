fbcc234c3a15458d9f0cdf9aaa30f3a956906d1b
unknown <jonathangoldnauvogt@Mealy.VLSI1.local>
2016-01-15 20:34:45 -0500

    final commit today

--- Software/Teensy Test Sketches/WavFilePlayer_modified/WavFilePlayer_modified.ino/WavFilePlayer_modified.ino.ino
+++ Software/Teensy Test Sketches/WavFilePlayer_modified/WavFilePlayer_modified.ino/WavFilePlayer_modified.ino.ino
@@ -1,35 +1,14 @@
-// Simple WAV file player example
-//
-// Three types of output may be used, by configuring the code below.
-//
-//   1: Digital I2S - Normally used with the audio shield:
-//         http://www.pjrc.com/store/teensy3_audio.html
-//
-//   2: Digital S/PDIF - Connect pin 22 to a S/PDIF transmitter
-//         https://www.oshpark.com/shared_projects/KcDBKHta
-//
-//   3: Analog DAC - Connect the DAC pin to an amplified speaker
-//         http://www.pjrc.com/teensy/gui/?info=AudioOutputAnalog
-//
-// To configure the output type, first uncomment one of the three
-// output objects.  If not using the audio shield, comment out
-// the sgtl5000_1 lines in setup(), so it does not wait forever
-// trying to configure the SGTL5000 codec chip.
-//
-// The SD card may connect to different pins, depending on the
-// hardware you are using.  Uncomment or configure the SD card
-// pins to match your hardware.
-//
-// Data files to put on your SD card can be downloaded here:
-//   http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
-//
-// This example code is in the public domain.
+
 
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
 
 AudioPlaySdWav           playWav1;
 // Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
@@ -45,14 +24,46 @@ AudioControlSGTL5000     sgtl5000_1;
 #define SDCARD_MOSI_PIN  11
 #define SDCARD_SCK_PIN   13
 
+#define NUMFLAKES 1
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
 // Use these for the SD+Wiz820 or other adaptors
 //#define SDCARD_CS_PIN    4
 //#define SDCARD_MOSI_PIN  11
 //#define SDCARD_SCK_PIN   13
 
+static const unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+uint8_t icons[NUMFLAKES][3];
+  
+  
 void setup() {
   Serial.begin(9600);
 
+  
+  
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
   AudioMemory(8);
@@ -70,8 +81,22 @@ void setup() {
     while (1) {
       Serial.println("Unable to access the SD card");
       delay(500);
+
+      
     }
   }
+
+  randomSeed(666);     // whatever seed
+ 
+  // initialize
+  for (uint8_t f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS] = random(display.width());
+    icons[f][YPOS] = 0;
+    icons[f][DELTAY] = random(5) + 1;
+
+  }
+
+
 }
 
 void playFile(const char *filename)
@@ -88,25 +113,39 @@ void playFile(const char *filename)
 
   // Simply wait for the file to finish playing.
   while (playWav1.isPlaying()) {
-    // uncomment these lines if you audio shield
-    // has the optional volume pot soldered
-    //float vol = analogRead(15);
-    //vol = vol / 1024;
-    // sgtl5000_1.volume(vol);
+   // 
+   AudioNoInterrupts();
+  display.begin();
+
+  display.setContrast(50);
+  
+  for (int16_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
+    display.display();
+  }
+
+    AudioInterrupts();
+delay(1000);
+      
+    //
   }
 }
 
 
 void loop() {
-  playFile("1.WAV");  // filenames are always uppercase 8.3 format
-  delay(500);
-  playFile("2.WAV");
-  delay(500);
-  playFile("STABLE.WAV");
-  delay(500);
+  //playFile("1.WAV");  // filenames are always uppercase 8.3 format
+  
+  //delay(500);
+  //playFile("2.WAV");
+  //delay(500);
+  //playFile("STABLE.WAV");
+  //delay(500);
   playFile("REWARD.WAV");
+  
   delay(1500);
-  playFile("GLITTER.WAV");
-  delay(1500);
+ // playFile("GLITTER.WAV");
+  //delay(1500);
 }
+
+
 
