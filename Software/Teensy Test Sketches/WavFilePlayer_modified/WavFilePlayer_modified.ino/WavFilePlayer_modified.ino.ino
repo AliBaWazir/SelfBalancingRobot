602ca6b2a62eb6fdcad5afc749f2e39bf2e992c6
unknown <jeysenfreedman@Station02.VLSI1.local>
2016-02-24 11:26:21 -0500

    Added digipot and LED code to Teensy Main Software folder

--- Software/Teensy Test Sketches/WavFilePlayer_modified/WavFilePlayer_modified.ino/WavFilePlayer_modified.ino.ino
+++ Software/Teensy Test Sketches/WavFilePlayer_modified/WavFilePlayer_modified.ino/WavFilePlayer_modified.ino.ino
@@ -1,14 +1,11 @@
+#include <Audio.h>
+#include <DAC.h>
 
 
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
-#include <SerialFlash.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_PCD8544.h>
-
-Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
 
 AudioPlaySdWav           playWav1;
 // Use one of these 3 output types: Digital I2S, Digital S/PDIF, or Analog DAC
@@ -24,45 +21,15 @@ AudioControlSGTL5000     sgtl5000_1;
 #define SDCARD_MOSI_PIN  11
 #define SDCARD_SCK_PIN   13
 
-#define NUMFLAKES 1
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-
-
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
 
 // Use these for the SD+Wiz820 or other adaptors
 //#define SDCARD_CS_PIN    4
 //#define SDCARD_MOSI_PIN  11
 //#define SDCARD_SCK_PIN   13
 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
-
-uint8_t icons[NUMFLAKES][3];
-  
   
 void setup() {
   Serial.begin(9600);
-
-  
   
   // Audio connections require memory to work.  For more
   // detailed information, see the MemoryAndCpuUsage example
@@ -86,16 +53,6 @@ void setup() {
     }
   }
 
-  randomSeed(666);     // whatever seed
- 
-  // initialize
-  for (uint8_t f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS] = random(display.width());
-    icons[f][YPOS] = 0;
-    icons[f][DELTAY] = random(5) + 1;
-
-  }
-
 
 }
 
@@ -113,21 +70,9 @@ void playFile(const char *filename)
 
   // Simply wait for the file to finish playing.
   while (playWav1.isPlaying()) {
-   // 
-   AudioNoInterrupts();
-  display.begin();
-
-  display.setContrast(50);
-  
-  for (int16_t i=0; i<display.height(); i+=2) {
-    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
-    display.display();
-  }
-
-    AudioInterrupts();
-delay(1000);
+ 
+  delay(1000);
       
-    //
   }
 }
 
@@ -143,8 +88,7 @@ void loop() {
   playFile("REWARD.WAV");
   
   delay(1500);
- // playFile("GLITTER.WAV");
-  //delay(1500);
+ 
 }
 
 
