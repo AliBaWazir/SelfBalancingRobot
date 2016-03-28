279fec7304f92fe8ae586933a239b9587fac0e80
Ali Ba Wazir <alwaziry20@gmail.com>
2016-03-28 00:10:55 -0400

    Changed parameter values for linear_sensor_array_driver

--- Software/Teensy Main Software/master/linear_sensor_array_driver.h
+++ Software/Teensy Main Software/master/linear_sensor_array_driver.h
@@ -5,10 +5,10 @@
 /****************************************************************************************
  * DEFINES AND TYPE DEFINITIONS
  ****************************************************************************************/
-#define BLACK_POINT_THRESHOLD 500
+#define BLACK_POINT_THRESHOLD 300
 #define WHITE_POINT_THRESHOLD 1000
 #define FRAME_BUFFER_LENGTH 128
-#define FRAME_BUFFER_MARGIN_LENGHT 0//10
+#define FRAME_BUFFER_MARGIN_LENGHT 10
 #define CONTRAST_MARGIN 8
 #define MAX_BLACK_LINES_PER_FRAME 3
 
